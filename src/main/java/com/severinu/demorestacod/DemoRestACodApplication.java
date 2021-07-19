f2d5efc325161f2dfb02f27e9625881d77fe27c1
severinum <sewerynmichota@gmail.com>
2021-07-19 19:32:20 +0100

    first commit

---
+++ src/main/java/com/severinu/demorestacod/DemoRestACodApplication.java
@@ -0,0 +1,13 @@
+package com.severinu.demorestacod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestACodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestACodApplication.class, args);
+    }
+
+}
